26b2910195a96e33f582022aa8a46d2ba911d6a2
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2024-03-24 22:49:12 +0300

    Добавил isLoad и isError

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,16 +1,25 @@
 import {  useEffect } from "react";
 import styles from "./MainPage.module.css";
-import { useAppDispatch } from "../../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { getData } from "../../store/sliceUrl";
 import MessageComponent from "../../components/MessageComponent/MessageComponent";
 import InputComponent from "../../components/InputComponent/InputComponent";
 
 const MainPage = () => {
   const dispatch = useAppDispatch();
+  const {isError, isLoad} = useAppSelector((state) => state.sliceUrl);
 
   useEffect(() => {
     dispatch(getData());
   }, []);
+
+  if(isLoad){
+    return <div className={styles.container}>Loading...</div>
+  }
+
+  if(isError){  
+    return <div className={styles.container}>Something went wrong</div>
+  }
 
   return (
     <div className={styles.container}>
